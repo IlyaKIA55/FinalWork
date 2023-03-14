92fd1d2494aec149e9c81b8cb494644f48b1e43f
Ilya <jyg3a125@gmail.com>
2023-03-14 13:56:12 +0500

    modified folder Program

---
+++ Program/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] FindLenght(string[] input)
+{
+    string[] output = new string[CountArray(input)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= 3)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountArray(string[] input)
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] Array()
+{
+    Console.Write("Введите значения через запятую: ");
+    return Console.ReadLine().Split(",");
+}
+
+string[] array = Array();
+string[] result = FindLenght(array);
+Console.WriteLine($"[{string.Join(", ", array)}] ---> [{string.Join(", ", result)}]");
